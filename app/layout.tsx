6add5a65f0d1a941ec36ba33a75570413428b7e6
Copple <10214025+kiwicopple@users.noreply.github.com>
2023-01-27 13:28:12 +0100

    init basic layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.css'
+import Sidebar from "@/components/Sidebar"
+import "./globals.css"
 
 export default function RootLayout({
   children,
@@ -12,7 +13,12 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Sidebar links={[]} />
+        <div className=" md:pl-64 flex min-h-0 flex-1 flex-col flex-grow h-full">
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { Alert } from "flowbite-react"
+
+export default function Hero() {
+  return (
+    <div>
+      <Alert color="info">Alert!</Alert>
+    </div>
+  )
+}
